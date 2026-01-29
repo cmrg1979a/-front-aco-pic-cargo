e1bcb241fb6dd7327405d454a4d1298438d6eaf0
Ledihas <chuchua441@gmail.com>
2026-01-29 05:59:38 +0000

    cambios | mejoraS

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0873ac14dd96be360843dbee7bcd2e38.js"
+  "/precache-manifest.fedeec502902290f83f43ad43b25ac8c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "aco_agente_aduana"});
